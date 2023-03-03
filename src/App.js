0b8208ce3bde5f9fe11d9a05711e7e0d8c100536
AlptugKizilay <alptugkizilay@gmail.com>
2023-03-04 00:29:12 +0300

    gorevler yapildi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Karakter from './components/Karakter'
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +11,33 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/', {
+
+    })
+      .then(function (response) {
+        console.log(response);
+        setChar(response.data);
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+
+  }, []);
+
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <div className='m-5'>
+        <h1 className="display-3">Karakterler</h1>
+        <Karakter char={char} />
+      </div>
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,74 @@
-// Karakter bileşeniniz buraya gelecek
+
+import React, { useState } from 'react';
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem,
+} from 'reactstrap';
+
+const Karakter = (props) => {
+    console.log(props.char);
+
+    const [open, setOpen] = useState('1');
+    const toggle = (id) => {
+        if (open === id) {
+            setOpen();
+        } else {
+            setOpen(id);
+        }
+    };
+    const [opener, setOpener] = useState('1');
+    const toggler = (id) => {
+        if (opener === id) {
+            setOpener();
+        } else {
+            setOpener(id);
+        }
+    };
+
+    return (
+        <div>
+            {props.char.map((char) => (
+                
+                <Accordion open={open} toggle={toggle} key={char.id}>
+                    <AccordionItem key={char.id}>
+                        <AccordionHeader targetId={char.name} key={char.id}  > {char.name} </AccordionHeader>
+                        <AccordionBody accordionId={char.name} key={char.id}>
+                            <strong>Gender:</strong> {char.gender} <br />
+                            <strong>Height:</strong> {char.height}<br />
+                            <strong>Mass:</strong> {char.mass}<br />
+                            <strong>BirthYear:</strong> {char.birth_year}<br />
+                            <strong>Eye Color:</strong>{char.eye_color}<br />
+                            <strong>Hair Color:</strong> {char.hair_color}<br />
+                            <strong>Skin Color:</strong> {char.skin_color}<br />
+                            
+
+                           <div>
+                                {char.films.map((film,index) => (
+                                    <Accordion open={opener} toggle={toggler} key={index}>
+                                        <AccordionItem key={index}>
+                                            <AccordionHeader targetId={film} key={index}> {film} </AccordionHeader>
+                                            <AccordionBody accordionId={film} key={index}>
+                                            {film}
+
+                                            </AccordionBody>
+
+                                        </AccordionItem>
+                                    </Accordion>
+                                ))}
+                            </div> 
+ 
+
+
+
+
+                        </AccordionBody>
+
+                    </AccordionItem>
+                </Accordion>
+            ))}
+        </div>
+    );
+}
+export default Karakter;
